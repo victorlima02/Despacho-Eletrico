5f494ac42328fe377876ce059b96004c36246b28
Victor <Victor@victor-note>
2014-07-13 16:36:59 -0300

    Commit inicial: propriedades do projeto.

--- src/simulacao/Simulacao.java
+++ src/simulacao/Simulacao.java
@@ -1,7 +1,30 @@
-/**
+/*
+ * The MIT License
  *
+ * Copyright 2014 Victor de Lima Soares.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
-package tcc;
+/**
+ * @author Victor de Lima Soares
+ */
+package simulacao;
 
 import ic.populacional.algoritmos.DE.DE;
 import ic.populacional.algoritmos.DE.motadores.Best;
@@ -38,20 +61,21 @@ public class Simulacao {
     Double demandaHoraria = 320d;
 
     public Simulacao() {
-        
+
         nExperimentos = 1;
-        
+
         maxIndividuos = 50;
         maxIteInteracoes = 50;
-        
+
         probabilidaDeCrossover = 0.5;
         fatorDePertubacao = 0.3;
         nDiferencas = 1;
-        
+
         usina = UsinaFactory.getUsina(UsinaFactory.Usinas.TRESMARIAS);
     }
 
     class Resultados {
+
         Long tempoExecucao;
         DistribuicaoVazao melhorSolucao;
     }
